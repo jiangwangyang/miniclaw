a43c87690661c91163f50932f2d3520fe5cab3a1
蒋汪洋 <402233792@qq.com>
2026-03-21 18:45:06 +0800

    feat: 增加飞书插件

---
+++ plugins/channel_feishu/plugin.py
@@ -0,0 +1,115 @@
+import json
+import logging
+import os
+import queue
+import threading
+import uuid
+
+import lark_oapi as lark
+import requests
+from fastapi import FastAPI
+from lark_oapi.api.im.v1 import *
+
+CHAT_URL = "http://localhost:11223/chat"
+INTERRUPT_URL = "http://localhost:11223/interrupt"
+LARK_APP_ID = os.getenv("LARK_APP_ID")
+LARK_APP_SECRET = os.getenv("LARK_APP_SECRET")
+
+message_queue = queue.Queue(maxsize=10)
+client = lark.Client.builder() \
+    .app_id(LARK_APP_ID) \
+    .app_secret(LARK_APP_SECRET) \
+    .log_level(lark.LogLevel.INFO) \
+    .build()
+
+
+# 发送飞书消息
+def send_feishu_message(receive_id: str, content: str):
+    request_body: CreateMessageRequestBody = CreateMessageRequestBody.builder() \
+        .receive_id(receive_id) \
+        .msg_type("text") \
+        .content(json.dumps({"text": content}, ensure_ascii=False)) \
+        .uuid(str(uuid.uuid4())) \
+        .build()
+    request: CreateMessageRequest = CreateMessageRequest.builder() \
+        .receive_id_type("open_id") \
+        .request_body(request_body) \
+        .build()
+    response: CreateMessageResponse = client.im.v1.message.create(request)
+    if not response.success():
+        lark.logger.error(f"client.im.v1.message.create failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}, resp: \n{json.dumps(json.loads(response.raw.content), ensure_ascii=False)}")
+
+
+# 用户消息消费者 将用户消息发送到模型
+def message_consumer():
+    while True:
+        try:
+            # 获取飞书消息
+            feishu_message = message_queue.get(timeout=1)
+            open_id = feishu_message.event.sender.sender_id.open_id
+            user_content = feishu_message.event.message.content
+            # 发送到模型
+            response = requests.post(f"{CHAT_URL}?id={open_id}&message={user_content}", stream=True)
+            response.raise_for_status()
+            # 遍历模型返回
+            for _ in response.iter_lines():
+                pass
+            # 处理完成
+            message_queue.task_done()
+        except queue.Empty:
+            pass
+
+
+# 飞书消息监听器 将飞书消息放入队列
+def event_listener():
+    def do_p2_im_message_receive_v1(data: lark.im.v1.P2ImMessageReceiveV1) -> None:
+        lark.logger.info(lark.JSON.marshal(data))
+        open_id = data.event.sender.sender_id.open_id
+        response = requests.post(f"{INTERRUPT_URL}?id={open_id}")
+        response.raise_for_status()
+        message_queue.put(data)
+
+    event_handler = lark.EventDispatcherHandler.builder("", "") \
+        .register_p2_im_message_receive_v1(do_p2_im_message_receive_v1) \
+        .build()
+    ws_client = lark.ws.Client(LARK_APP_ID, LARK_APP_SECRET, event_handler=event_handler, log_level=lark.LogLevel.INFO)
+    ws_client.start()
+
+
+async def before_application(app: FastAPI, **kwargs):
+    # 启动消费者线程
+    message_consumer_thread = threading.Thread(target=message_consumer, args=())
+    message_consumer_thread.start()
+    # 事件监听客户端
+    event_listener_thread = threading.Thread(target=event_listener, args=())
+    event_listener_thread.start()
+    # 记录日志
+    logging.info("Feishu plugin started")
+
+
+async def after_application(app: FastAPI, **kwargs):
+    logging.info("Feishu plugin stopped")
+
+
+async def before_chat(session_id: str, messages: list, user_content: str, **kwargs):
+    pass
+
+
+async def after_chat(session_id: str, messages: list, user_content: str, assistant_content: str, **kwargs):
+    pass
+
+
+async def before_model(session_id: str, messages: list, **kwargs):
+    pass
+
+
+async def after_model(session_id: str, messages: list, **kwargs):
+    send_feishu_message(session_id, messages[-1]["content"])
+
+
+async def before_tool(session_id: str, messages: list, tool_call: dict, **kwargs):
+    pass
+
+
+async def after_tool(session_id: str, messages: list, tool_call: dict, tool_content: str, **kwargs):
+    send_feishu_message(session_id, json.loads(tool_call["function"]["arguments"])["command"] + "\n" + tool_content)
