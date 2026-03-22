ae6ce10ae8b1d70e909205613f91fae9f902ae3c
蒋汪洋 <402233792@qq.com>
2026-03-22 12:12:51 +0800

    fix: 修复飞书启动bug

--- plugins/channel_feishu/plugin.py
+++ plugins/channel_feishu/plugin.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import logging
 import os
@@ -14,34 +15,35 @@ CHAT_URL = "http://localhost:11223/chat"
 INTERRUPT_URL = "http://localhost:11223/interrupt"
 LARK_APP_ID = os.getenv("LARK_APP_ID")
 LARK_APP_SECRET = os.getenv("LARK_APP_SECRET")
-
 message_queue = queue.Queue(maxsize=10)
-client = lark.Client.builder() \
-    .app_id(LARK_APP_ID) \
-    .app_secret(LARK_APP_SECRET) \
-    .log_level(lark.LogLevel.INFO) \
-    .build()
-
-
-# 发送飞书消息
-def send_feishu_message(receive_id: str, content: str):
-    request_body: CreateMessageRequestBody = CreateMessageRequestBody.builder() \
-        .receive_id(receive_id) \
-        .msg_type("text") \
-        .content(json.dumps({"text": content}, ensure_ascii=False)) \
-        .uuid(str(uuid.uuid4())) \
-        .build()
-    request: CreateMessageRequest = CreateMessageRequest.builder() \
-        .receive_id_type("open_id") \
-        .request_body(request_body) \
-        .build()
-    response: CreateMessageResponse = client.im.v1.message.create(request)
-    if not response.success():
-        lark.logger.error(f"client.im.v1.message.create failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}, resp: \n{json.dumps(json.loads(response.raw.content), ensure_ascii=False)}")
 
 
 # 用户消息消费者 按序消费飞书消息 将用户消息发送到模型并回复
 def message_consumer():
+    # 初始化飞书客户端
+    client = lark.Client.builder() \
+        .app_id(LARK_APP_ID) \
+        .app_secret(LARK_APP_SECRET) \
+        .log_level(lark.LogLevel.INFO) \
+        .build()
+
+    # 发送飞书消息
+    def send_feishu_message(receive_id: str, content: str):
+        request_body: CreateMessageRequestBody = CreateMessageRequestBody.builder() \
+            .receive_id(receive_id) \
+            .msg_type("text") \
+            .content(json.dumps({"text": content}, ensure_ascii=False)) \
+            .uuid(str(uuid.uuid4())) \
+            .build()
+        request: CreateMessageRequest = CreateMessageRequest.builder() \
+            .receive_id_type("open_id") \
+            .request_body(request_body) \
+            .build()
+        response: CreateMessageResponse = client.im.v1.message.create(request)
+        if not response.success():
+            lark.logger.error(f"client.im.v1.message.create failed, code: {response.code}, msg: {response.msg}, log_id: {response.get_log_id()}, resp: \n{json.dumps(json.loads(response.raw.content), ensure_ascii=False)}")
+
+    # 死循环消费队列飞书消息
     while True:
         try:
             # 获取飞书消息
@@ -86,14 +88,21 @@ def event_listener():
     event_handler = lark.EventDispatcherHandler.builder("", "") \
         .register_p2_im_message_receive_v1(do_p2_im_message_receive_v1) \
         .build()
+    # 启动新的事件循环创建监听客户端(因为miniclaw已经占用了事件循环，所以需要创建一个新的)
+    lark.ws.client.loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(lark.ws.client.loop)
     ws_client = lark.ws.Client(LARK_APP_ID, LARK_APP_SECRET, event_handler=event_handler, log_level=lark.LogLevel.INFO)
     ws_client.start()
 
 
 async def before_application(app: FastAPI, **kwargs):
+    if not LARK_APP_ID or not LARK_APP_SECRET:
+        return
     # 启动 消费者线程 消息监听线程
     message_consumer_thread = threading.Thread(target=message_consumer, args=())
     event_listener_thread = threading.Thread(target=event_listener, args=())
+    message_consumer_thread.daemon = True
+    event_listener_thread.daemon = True
     message_consumer_thread.start()
     event_listener_thread.start()
     logging.info("Feishu plugin started")
