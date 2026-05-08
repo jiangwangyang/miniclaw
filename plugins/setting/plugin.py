5ef7cb44c3bc4c5ca268102cf5bf9ba3e4183af2
jiangwangyang <402233792@qq.com>
2026-05-08 13:09:31 +0800

    feat: Improve settings handling
    Refactor get_settings and save_settings functions to handle empty settings file and change content type to dict.

--- plugins/setting/plugin.py
+++ plugins/setting/plugin.py
@@ -20,18 +20,19 @@ async def init_settings_file():
 @ROUTER.get("")
 async def get_settings():
     settings_file = anyio.Path(SETTINGS_FILE)
-    if not await settings_file.exists():
-        return {}
-    content = await settings_file.read_text(encoding="utf-8")
-    return json.loads(content)
+    content = await settings_file.read_text(encoding="utf-8") if await settings_file.exists() else ""
+    return json.loads(content) if content else {}
 
 
 @ROUTER.post("")
-async def save_settings(content: str = Body(...)):
+async def save_settings(content: dict = Body(...)):
     settings_file = anyio.Path(SETTINGS_FILE)
     await settings_file.parent.mkdir(parents=True, exist_ok=True)
-    content = json.dumps(json.loads(content), ensure_ascii=False, indent=4)
-    await settings_file.write_text(content, encoding="utf-8")
+    file_content = await settings_file.read_text(encoding="utf-8") if await settings_file.exists() else ""
+    file_json = json.loads(file_content) if file_content else {}
+    file_json.update(content)
+    file_content = json.dumps(file_json, ensure_ascii=False, indent=4)
+    await settings_file.write_text(file_content, encoding="utf-8")
 
 
 @asynccontextmanager
