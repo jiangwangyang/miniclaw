401269c5ed0674610faae30ebafde8d6a53fdee8
jiangwangyang <402233792@qq.com>
2026-05-08 14:37:09 +0800

    feat: Configure uvicorn to run with 1 worker and no access log
    Added options for workers and access log to uvicorn run.

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ import uvicorn
 
 if __name__ == "__main__":
     os.chdir(pathlib.Path(__file__).parent)
-    uvicorn.run("miniclaw:app", host="localhost", port=11223)
+    uvicorn.run("miniclaw:app", host="localhost", port=11223, workers=1, access_log=False)
