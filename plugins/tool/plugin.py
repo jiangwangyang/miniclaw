5d995d539ac0ae367095c850b64d7f7fb481600a
蒋汪洋 <402233792@qq.com>
2026-05-08 20:34:53 +0800

    feat: 合并shell和python工具插件

--- plugins/tool/plugin.py
+++ plugins/tool/plugin.py
@@ -53,21 +53,56 @@ WRITE_FILE_TOOL = {
         "required": ["path", "content"]
     }
 }
+PYTHON_FILE_TOOL = {
+    "name": "python_file",
+    "description": f"Run python program read from script file. Python version: {platform.python_version()}.",
+    "input_schema": {
+        "type": "object",
+        "properties": {
+            "file": {
+                "type": "string",
+                "description": "python file path"
+            },
+            "args": {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                },
+                "description": "arguments passed to program in sys.argv[1:]"
+            }
+        },
+        "required": ["file"]
+    }
+}
+PYTHON_CMD_TOOL = {
+    "name": "python_cmd",
+    "description": f"Run python program passed in as string. Python version: {platform.python_version()}.",
+    "input_schema": {
+        "type": "object",
+        "properties": {
+            "cmd": {
+                "type": "string",
+                "description": "python command"
+            }
+        },
+        "required": ["cmd"]
+    }
+}
 
 
 @asynccontextmanager
 async def lifespan(**kwargs):
     if sys.platform.startswith("win"):
-        logging.info("Shell tool plugin started, having 2 tools: read_file, write_file")
+        logging.info("Tool plugin started, having 4 tools: read_file, write_file, python_file, python_cmd")
     else:
-        logging.info("Shell tool plugin started, having 1 tool: shell")
+        logging.info("Tool plugin started, having 1 tool: shell")
     yield
-    logging.info("Shell tool plugin stopped")
+    logging.info("Tool plugin stopped")
 
 
 async def before_chat(tools: list, **kwargs):
     if sys.platform.startswith("win"):
-        tools += [READ_FILE_TOOL, WRITE_FILE_TOOL]
+        tools += [READ_FILE_TOOL, WRITE_FILE_TOOL, PYTHON_FILE_TOOL, PYTHON_CMD_TOOL]
     else:
         tools += [SHELL_TOOL]
 
@@ -88,9 +123,20 @@ async def before_tool(messages: list, tool_call: dict, work_dir: str, **kwargs):
             path, content = tool_call["input"].get("path", ""), tool_call["input"].get("content", "")
             await anyio.Path(path).write_text(content, encoding="utf-8")
             tool_content, is_error = "write success", False
+        elif tool_call["name"] == "python_file":
+            python_file, args = tool_call["input"].get("file", ""), tool_call["input"].get("args", [])
+            process = await asyncio.create_subprocess_exec(sys.executable, python_file, *args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=work_dir)
+            stdout, stderr = await process.communicate()
+            tool_content = f"{stdout.decode("utf-8", errors="replace")}{stderr.decode("utf-8", errors="replace")}"
+            is_error = process.returncode != 0
+        elif tool_call["name"] == "python_cmd":
+            python_cmd = tool_call["input"].get("cmd", "")
+            process = await asyncio.create_subprocess_exec(sys.executable, "-c", python_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=work_dir)
+            stdout, stderr = await process.communicate()
+            tool_content = f"{stdout.decode("utf-8", errors="replace")}{stderr.decode("utf-8", errors="replace")}"
+            is_error = process.returncode != 0
         else:
             return
     except Exception as e:
-        tool_content = f"Error: {e}"
-        is_error = True
+        tool_content, is_error = f"Error: {e}", True
     messages[-1]["content"] += [{"type": "tool_result", "tool_use_id": tool_call["id"], "content": tool_content, "is_error": is_error}]
